19f4071dc9694ebe644117c22b4184258db3bc42
xiaoniu <270146073@qq.com>
2024-03-25 09:43:16 +0800

    test

--- 08_webpack自定义plugins/plugins/AutoUploadWebpackPlugin.js
+++ 08_webpack自定义plugins/plugins/AutoUploadWebpackPlugin.js
@@ -1,7 +1,19 @@
+const { NodeSSH } = require("node-ssh")
 class AutoUploadWebpackPlugin {
     apply(complier) {
-        console.log("AutoUploadWebpackPlugin组件被注册")
+        // console.log("AutoUploadWebpackPlugin组件被注册")
+        complier.hooks.afterEmit.tapAsync("AutoPlugin", async (compilation, callback) => {
+            // 获取输出文件夹路径
+            const outputPath = compilation.outputOptions.path
+            console.log(outputPath)
+
+            // await this.connectServer()
+            callback()
+        })
     }
+
+    // 连接远程服务器
+
 }
 
 module.exports = AutoUploadWebpackPlugin
